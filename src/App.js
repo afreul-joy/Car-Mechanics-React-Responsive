0b5bb58ab9eda94202af7619a7fa1536661e7b5a
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-25 16:37:33 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/Home">
+           <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
