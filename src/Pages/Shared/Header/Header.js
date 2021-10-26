0e80da87d02c8a1e4ba4cd73ef83f64216deedb3
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-26 16:11:58 +0600

    car mechanics google sign in done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Container,Nav,Navbar} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import useAuth from '../../../hooks/useAuth';
 
 const Header = () => {
@@ -14,11 +15,11 @@ const Header = () => {
             <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">          
               {/* <Nav.Link href="/home#home">Home</Nav.Link> */}
-            <Nav.Link as={Link} to="/home#home">Home</Nav.Link>     
-              <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
-              <Nav.Link as={Link} to="/home#experts">Experts</Nav.Link>
+            <Nav.Link as={HashLink} to="/home#home" className="btn btn-outline-primary text-light" >Home</Nav.Link>     
+              <Nav.Link as={HashLink} to="/home#services" className="btn btn-outline-primary text-light">Services</Nav.Link>
+              <Nav.Link as={HashLink} to="/home#experts" className="btn btn-outline-primary text-light">Experts</Nav.Link>
             {user?.email ?
-            <button onClick={logOut} type="button" class="btn btn-light">LogOut</button> :
+            <button onClick={logOut} type="button" class="btn btn-light mx-3 ">LogOut</button> :
               <Nav.Link as={Link} to="/login">login</Nav.Link>}
                 <Navbar.Text>
               Signed in as: <a href="#login">{user?.displayName}</a>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -7,15 +7,19 @@ initializeAuthentication();
 
 const useFirebase = () => {
   const [user, setUser] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  
   const auth = getAuth();
 
   const signInUsingGoogle = () => {
+    setIsLoading(true)
     const googleProvider = new GoogleAuthProvider();
     signInWithPopup(auth, googleProvider)
       .then((result) => {
         setUser(result.user);
   
       })
+    .finally(()=>setIsLoading(false))
   }
   // observe user state change অন্য ট্যাবে গেলেও যাতে sync থাকে   
     useEffect(() => {
@@ -24,20 +28,24 @@ const useFirebase = () => {
           setUser(user);
         } else {
           setUser({})
-        }
+       }
+       setIsLoading(false);
       });
       return () => unsubscribed;
     },[])
     
-    const logOut = () => {
+  const logOut = () => {
+      setIsLoading(true)
       signOut(auth)
         .then(() => { })
+        .finally(()=>setIsLoading(false))
     }
   
   return {
     user,
+    isLoading,
     signInUsingGoogle,
-    logOut
+    logOut,
   }
 }
 
